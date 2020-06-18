a40823026efdb02c6d7d9a4368e957fb19a4a786
Robert Bastian <r.bastian.2@gmail.com>
2020-06-18 10:37:54 -0500

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -8,8 +8,7 @@ class Quiz extends Component {
         super(props);
         this.state = {quiz_position: 1};
     }
-    render() {
-        
+    render() {       
         return (
             <div>
                 <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
